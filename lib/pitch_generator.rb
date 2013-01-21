edd594c6cdaa4ab033815d15ec2d6fc92183a3cf
Jim Lindstrom <jim.lindstrom@gmail.com>
2013-01-20 21:23:16 -0800

    removed references to custom critics. that is a good idea, but not yet.

--- lib/pitch_generator.rb
+++ lib/pitch_generator.rb
@@ -1,26 +1,5 @@
 #!/usr/bin/env ruby
 
-class CustomPitchCritic < PitchCritic
-  def load(folder)
-    # do nothing, because this one is custom, per-song
-  end
-end
-class CustomIntervalCritic < IntervalCritic
-  def load(folder)
-    # do nothing, because this one is custom, per-song
-  end
-end
-class CustomPitchAndPitchClassSetCritic < PitchAndPitchClassSetCritic
-  def load(folder)
-    # do nothing, because this one is custom, per-song
-  end
-end
-class CustomComplexPitchCritic < ComplexPitchCritic
-  def load(folder)
-    # do nothing, because this one is custom, per-song
-  end
-end
-
 class PitchGenerator
   def initialize
     @pitch_critic = PitchCritic.new(order=2)
@@ -34,21 +13,6 @@ class PitchGenerator
                  @interval_critic,
                  @pitch_and_pitch_class_set_critic,
                  @complex_pitch_critic ]
-
-    # I'm guessing this was an attempt to allow each song to exhibit its own idioms & 
-    # tendencies, which could get mixed in with more global expectations. For now, though
-    # I'm shutting it off.
-    #@custom_pitch_critic = CustomPitchCritic.new(order=2)
-    #@custom_interval_critic = CustomIntervalCritic.new(order=3, lookahead=3)
-    #@custom_pitch_and_pitch_class_set_critic = CustomPitchAndPitchClassSetCritic.new(order=3, lookahead=3)
-    #@custom_complex_pitch_critic = CustomComplexPitchCritic.new(@custom_pitch_critic, 
-    #                                               @custom_interval_critic, 
-    #                                               @custom_pitch_and_pitch_class_set_critic)
-
-    #@critics +=[ @custom_pitch_critic,
-    #             @custom_interval_critic,
-    #             @custom_pitch_and_pitch_class_set_critic,
-    #             @custom_complex_pitch_critic ]
   end
 
   def critics

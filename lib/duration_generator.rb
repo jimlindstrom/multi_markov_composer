7097c26a64e462cf01e23a76183dd7602085b06f
Jim Lindstrom <jim.lindstrom@gmail.com>
2013-01-20 21:22:36 -0800

    removed references to custom critics. that is a good idea, but not yet.

--- lib/duration_generator.rb
+++ lib/duration_generator.rb
@@ -1,45 +1,16 @@
 #!/usr/bin/env ruby
 
-class CustomDurationCritic < DurationCritic
-  def load(folder)
-    # do nothing
-  end
-end
-class CustomDurationAndBeatPositionCritic < DurationAndBeatPositionCritic
-  def load(folder)
-    # do nothing
-  end
-end
-class CustomComplexDurationCritic < ComplexDurationCritic
-  def load(folder)
-    # do nothing
-  end
-end
-
 class DurationGenerator
   def initialize
     @duration_critic = DurationCritic.new(order=3)
-    #@duration_and_beat_position_critic = DurationAndBeatPositionCritic.new(order=2, lookahead=3)
-    #@complex_duration_critic = 
-    #  ComplexDurationCritic.new(@duration_critic, 
-    #                            @duration_and_beat_position_critic)
+    @duration_and_beat_position_critic = DurationAndBeatPositionCritic.new(order=2, lookahead=3)
+    @complex_duration_critic = 
+      ComplexDurationCritic.new(@duration_critic, 
+                                @duration_and_beat_position_critic)
 
-    @critics = [ @duration_critic ]#,
-    #             @duration_and_beat_position_critic,
-    #             @complex_duration_critic ]
-
-    # I'm guessing this was an attempt to allow each song to exhibit its own idioms & 
-    # tendencies, which could get mixed in with more global expectations. For now, though
-    # I'm shutting it off.
-    #@custom_duration_critic = CustomDurationCritic.new(order=3)
-    #@custom_duration_and_beat_position_critic = CustomDurationAndBeatPositionCritic.new(order=2, lookahead=3)
-    #@custom_complex_duration_critic = 
-    #  CustomComplexDurationCritic.new(@custom_duration_critic, 
-    #                            @custom_duration_and_beat_position_critic)
-
-    #@critics+= [ @custom_duration_critic,
-    #             @custom_duration_and_beat_position_critic,
-    #             @custom_complex_duration_critic ]
+    @critics = [ @duration_critic,
+                 @duration_and_beat_position_critic,
+                 @complex_duration_critic ]
   end
 
   def critics
@@ -51,8 +22,7 @@ class DurationGenerator
   end
 
   def generate
-    #expectations = @complex_duration_critic.expectations
-    expectations = @duration_critic.expectations
+    expectations = @complex_duration_critic.expectations
     x = expectations.sample
     return MusicIR::Duration.new(x) if !x.nil?
 

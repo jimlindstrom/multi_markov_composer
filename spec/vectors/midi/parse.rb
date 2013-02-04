cca496438595f10f92dfc8b36f48aa21c9914466
Jim Lindstrom <jim.lindstrom@gmail.com>
2013-02-03 22:21:06 -0500

    Now includes more info about the song's key and time signatures

--- spec/vectors/midi/parse.rb
+++ spec/vectors/midi/parse.rb
@@ -14,43 +14,65 @@ File.open(ARGV[0], 'rb') do |file|
   end
 end
 
-puts "\t\"File: #{ARGV[0]}\" =>"
-puts "\t{"
 
-#majorkeys = ['c flat', 'g flat', 'd flat', 'a flat', 'e flat', 'b flat', 'f', 'c', 'g', 'd', 'a', 'e', 'b', 'f sharp', 'c sharp']
-#minorkeys = ['a flat', 'e flat', 'b flat', 'f', 'c', 'g', 'd', 'a', 'e', 'b', 'f sharp', 'c sharp', 'g sharp', 'd sharp', 'a sharp']
-#
-#seq.tracks[0].events.each do |e|
-#  if e.respond_to?(:minor_key?) # what if there are two of these??
-#    is_major = (e.major_key?)     ? "true" : "false"
-#    is_sharp = (e.sharpflat >= 0) ? "true" : "false"
-#
-#    if e.major_key?
-#      pitch_class = majorkeys[e.sharpflat + 7]
-#    else
-#      pitch_class = minorkeys[e.sharpflat + 7]
-#    end
-#
-#    puts "\t\t:key_sig => Music::KeySignature.new(is_major=#{is_major}, is_sharp=#{is_sharp}, pitch_class=\"#{pitch_class}\"),"
-#  end
-#end
+if seq.tracks[0].events.select{ |e| e.respond_to?(:minor_key?) }.length != 1
+  raise RuntimeError.new("Has more than one key signiture")
+end
+if seq.tracks[0].events.select{ |e| e.respond_to?(:denominator) }.length != 1
+  raise RuntimeError.new("Has more than one time signiture")
+end
+if seq.tracks[0].events.select{ |e| e.respond_to?(:tempo) }.length != 1
+  raise RuntimeError.new("Has more than one tempo")
+end
 
-puts "\t\t:events =>"
-puts "\t\t["
+
+puts "  \"File: #{ARGV[0]}\" =>"
+puts "  {"
+
+puts "    :ppqn => #{seq.ppqn},"
+
+majorkeys = ['Cb', 'Gb', 'Db', 'Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#']
+minorkeys = ['Ab', 'Eb', 'Bb', 'F', 'C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#', 'G#', 'D#', 'A#']
+
+seq.tracks[0].events.each do |e|
+  if e.respond_to?(:minor_key?) # what if there are two of these??
+    mode = (e.major_key?) ? :major : :minor
+
+    if e.major_key?
+      pitch_class = majorkeys[e.sharpflat + 7]
+    else
+      pitch_class = minorkeys[e.sharpflat + 7]
+    end
+
+    puts "    :key_sig => MusicIR::Chord.new(MusicIR::PitchClass.from_s(\"#{pitch_class}\"), :#{mode}),"
+
+  elsif e.respond_to?(:denominator)
+    puts "    :time_sig_numerator => #{e.numerator},"
+    puts "    :time_sig_denominator => #{2**e.denominator},"
+    puts "    :time_sig_ticks => #{e.metronome_ticks}, # ticks (timestamp units) / quarter note"
+
+  elsif e.respond_to?(:tempo)
+    puts "    :tempo => #{e.tempo}, # msec / quarter note"
+  end
+end
+
+
+puts "    :events =>"
+puts "    ["
 
 seq.tracks[1].events.each do |e|
   timestamp = e.time_from_start
   if e.respond_to?(:off)
     pitch    = e.note
     velocity = e.velocity
-    puts "\t\t\tMidi::NoteOnEvent.new({:pitch => #{pitch}, :velocity => #{velocity}, :timestamp => #{timestamp}}),"
+    puts "      MusicIR::NoteOnEvent.new({:pitch => #{pitch}, :velocity => #{velocity}, :timestamp => #{timestamp}}),"
   elsif e.respond_to?(:on)
     pitch    = e.note
     velocity = e.velocity
-    puts "\t\t\tMidi::NoteOffEvent.new({:pitch => #{pitch}, :velocity => #{velocity}, :timestamp => #{timestamp}}),"
+    puts "      MusicIR::NoteOffEvent.new({:pitch => #{pitch}, :velocity => #{velocity}, :timestamp => #{timestamp}}),"
   end
 end
 
 
-puts "\t\t],"
-puts "\t},"
+puts "    ],"
+puts "  },"
